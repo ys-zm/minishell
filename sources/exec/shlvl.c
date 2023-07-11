ff48782ba64a781a8c62aa13abca3d6b243a3b55
Yesim Zaim <yzaim@f1r4s15.codam.nl>
2023-07-11 18:57:09 +0200

    norm fixed

--- sources/exec/shlvl.c
+++ sources/exec/shlvl.c
@@ -6,28 +6,43 @@
 /*   By: yzaim <marvin@codam.nl>                      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/26 14:05:32 by yzaim         #+#    #+#                 */
-/*   Updated: 2023/07/11 18:06:44 by yzaim         ########   odam.nl         */
+/*   Updated: 2023/07/11 18:56:30 by yzaim         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell/minishell.h"
 
+int	ft_check_if_digits(char *str)
+{
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			break ;
+		str++;
+	}
+	if (*str != '\0')
+		return (true);
+	return (false);
+}
+
 t_shlvl_status	ft_check_val_shlvl(char *val)
 {
 	long				value;
 	t_shlvl_status		ret;
-	
+
 	ret = -1;
 	value = ft_atol(val);
-	if (value > INT_MIN && value < 0)
+	if (value < 0)
 		ret = SET_ZERO;
 	else if (value == 999)
 		ret = SET_NULL;
+	else if (!ft_check_if_digits(val) && (value > INT_MAX || value < INT_MIN))
+		ret = SET_ONE;
 	else if (value > 999)
 		ret = OUT_OF_BOUNDS; 
-	else if (value < INT_MAX)
-		ret = SET_ONE;
-	else if (value > 0 && value < 999)
+	else if (value >= 0 && value < 999)
 		ret = INCREMENT;
 	return (ret);
 }
@@ -51,27 +66,27 @@ void	ft_update_shlvl(t_shlvl_status ret, t_env *env)
 		ft_change_node(env, NULL);
 	else if (ret == INCREMENT)
 	{
-		value = ft_atoi(env->value);
-		value +=1;
+		value = ft_atoi(env->value) + 1;
 		free(env->value);
 		env->value = ft_itoa(value);
 	}
 	else if (ret == OUT_OF_BOUNDS)
 	{
-		ft_write_error(2, NULL, "warning", "shell level too high, resetting to 1");
+		ft_change_node(env, "1");
+		ft_write_error(2, NULL, "warning", \
+			"shell level too high, resetting to 1");
 	}
 	else
 	{
 		ft_write_error(2, NULL, NULL, "error when setting shell level");
 	}
-	
 }
 
 void	ft_set_shlvl(t_var *mini)
 {
 	t_env			*env;
 	t_shlvl_status	ret;
-	
+
 	env = *(mini->env_list);
 	ret = -1;
 	if (ft_check_if_key_exists(*(mini->env_list), "SHLVL"))
@@ -84,7 +99,7 @@ void	ft_set_shlvl(t_var *mini)
 		}
 		ret = ft_check_val_shlvl(env->value);
 		if (ret >= 0)
-			ft_update_shlvl(ret, env);	
+			ft_update_shlvl(ret, env);
 	}
 	else
 		ft_add_node(mini->env_list, ft_new_node("SHLVL", "1"));
